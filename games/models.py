c32dcbf323daf50600ee525b96ced7b980d94887
wismo-s <stepha2128.af@gmail.com>
2023-10-15 15:55:31 -0500

    models games, genders, developers

--- games/models.py
+++ games/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+#Create your models here.
+
+class Developers(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+
+    def str(self):
+        return self.name
+
+class Genders(models.Model):
+    title = models.CharField(max_length=100, unique=True, null=False, blank=False)
+
+    def str(self):
+        return self.title
+
+class Games(models.Model):
+    title = models.CharField(max_length=200, unique=True, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    developer = models.ForeignKey(Developers, on_delete=models.CASCADE, null=False, blank=False)
+    date_realise = models.DateField(null=False)
+    sellers = models.IntegerField()
+    calification = models.IntegerField()
+    gender = models.ManyToManyField(Genders)
+
+    def str(self):
+        return f"{self.title} -> {self.developer}"
