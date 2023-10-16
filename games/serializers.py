71c0a99ff097ab634af78c2ccd5d98f8fa1a2644
wismo-s <stepha2128.af@gmail.com>
2023-10-16 10:14:25 -0500

    create viewsets

--- games/serializers.py
+++ games/serializers.py
@@ -5,3 +5,13 @@ class GamesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Games
         fields = "__all__"
+
+class DevelopersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Developers
+        fields = "__all__"
+        
+class GendersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genders
+        fields = "__all__"
--- games/views.py
+++ games/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Games, Developers, Genders
+from .serializers import GamesSerializer, GendersSerializer, DevelopersSerializer
 
 # Create your views here.
+
+class GamesViewSet(viewsets.ModelViewSet):
+    serializer_class = GamesSerializer
+    queryset = Games.objects.all()
+    
+class DeveloperViewSet(viewsets.ModelViewSet):
+    serializer_class = DevelopersSerializer
+    queryset = Developers.objects.all()
+
+class GendersViewSet(viewsets. ModelViewSet):
+    serializer_class = GendersSerializer
+    queryset = Genders.objects.all()
