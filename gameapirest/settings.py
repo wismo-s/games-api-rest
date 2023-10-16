fd6b90a2a9c2be3b4e81043839c44dd878636b78
wismo-s <stepha2128.af@gmail.com>
2023-10-16 11:15:57 -0500

    add documentation

--- gameapirest/settings.py
+++ gameapirest/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'coreapi',
     'games'
 ]
 
@@ -128,3 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOWED_ORIGINS = []
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+}
--- games/urls.py
+++ games/urls.py
@@ -1,4 +1,5 @@
 from rest_framework import routers
+from rest_framework.documentation import include_docs_urls
 from django.urls import path, include
 from games import views
 
@@ -10,5 +11,6 @@ router.register(r'genders', views.GendersViewSet, basename="genders")
 
 
 urlpatterns = [
-    path('api/V1/', include(router.urls))
+    path('api/V1/', include(router.urls)),
+    path('docs/', include_docs_urls(title="GamesAPI"))
 ]
