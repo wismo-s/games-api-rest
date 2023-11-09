ee107976bdc7ba0a04c74205a53012c3a1e8a209
wismo-s <stepha2128.af@gmail.com>
2023-11-09 12:05:01 -0500

    allowed versel

--- gameapirest/settings.py
+++ gameapirest/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "http://0.0.0.0", "http://localhost", "http://127.0.0.1"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "http://0.0.0.0", "http://localhost", "http://127.0.0.1, https://games-api-rest.vercel.app"]
 
 CORS_ALLOW_CREDENTIALS = True
 
